7e32d8adc6597f1e61d491bf0e88f142cac56972
Anna Guidi <guidi.a@husky.neu.edu>
2016-04-17 17:28:03 -0400

    fix hound errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   after_create :build_default_profile
 
   private
