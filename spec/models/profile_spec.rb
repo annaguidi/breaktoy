fe5f0f1118633872b8df25512f4b3bd8ee23f316
Anna Guidi <guidi.a@husky.neu.edu>
2016-04-17 20:06:16 -0400

    add unit test for profiles

---
+++ spec/models/profile_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Profile, type: :model do
+  let(:profile) do
+    Profile.new(
+      name: "socr8",
+      avatar_url: "http://i.imgur.com/QpC6Dpl.jpg",
+      location: "Ancient Greece",
+      about: "Be as you wish to Github"
+    )
+  end
+
+  describe ".new" do
+    it "should be an Profile object" do
+      expect(profile).to be_a(Profile)
+    end
+  end
+
+end
