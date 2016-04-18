8fe3707a0be408b3b1a934309db74c02ed398da2
Anna Guidi <guidi.a@husky.neu.edu>
2016-04-17 23:02:48 -0400

    add basic user auth & dependent destroy on profiles

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
   end
 
   factory :profile do
-    sequence(:name) { |n| "Angelica{n}" }
+    sequence(:name) { |n| "Angelica#{n}" }
     location "Boston"
     avatar_url "https://s-media-cache-ak0.pinimg.com/236x/50/bb/11/50bb1149e480e46d721d8a813a8ef3d4.jpg"
     about "Isn't this punny?!"
