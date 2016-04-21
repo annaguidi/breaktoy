32b4ddb9a523eccae1defabec1190de1c37c4cef
Anna Guidi <guidi.a@husky.neu.edu>
2016-04-21 14:22:16 -0400

    fix API call and user can delete

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,7 @@
 class Member < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
-  has_many :markers
+  has_many :markers, dependent: :destroy
 
   validates :group, presence: true
   validates :user, presence: true
