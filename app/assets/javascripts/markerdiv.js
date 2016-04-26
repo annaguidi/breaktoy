31cbea9e00285e71edeba267eb5435f48433f85f
Anna Guidi <guidi.a@husky.neu.edu>
2016-04-25 20:49:20 -0400

    scroll bar works really well, more styling

--- app/assets/javascripts/markerdiv.js
+++ app/assets/javascripts/markerdiv.js
@@ -1,4 +1,5 @@
 var group_id = $("#data-group-id").data("group-id");
+
 function initScroll() {
   $.ajax({
     type: 'GET',
@@ -8,10 +9,21 @@ function initScroll() {
     success: function(response) {
       var scrollbar = $(document.getElementById("marker-sidebar"));
       for (var i = response.length - 1; i > 0; i--) {
-        scrollbar.append('<p>' + response[i].title + '</p>');
+        scrollbar.append("<a href='#' class='link-btn' data-marker-latitude='"
+        + response[i].latitude + "' + data-marker-longitude='" +
+        response[i].longitude + "'>" + response[i].title + "</a><br>"
+        + "- " + response[i].user + "<br><br>");
       }
     }
   });
 }
+
+$(document).on('click', '.link-btn', function PanOnMarker() {
+  var longitude = $(this).data("marker-longitude");
+  var latitude = $(this).data("marker-latitude");
+
+  map.setCenter(new google.maps.LatLng(latitude, longitude));
+  map.setZoom(18);
+});
 
 google.maps.event.addDomListener(window, 'load', initScroll);
