d3676abd8d2afd9bdbc0c4d8c893a96be40d6bb7
Anna Guidi <guidi.a@husky.neu.edu>
2016-04-28 20:21:01 -0400

    logo fixed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 CarrierWave.configure do |config|
   if !Rails.env.test?
-    config.storage = :fog
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
